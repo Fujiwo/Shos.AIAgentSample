eb44f173f0c63620e8cb40bf2f9d0fb79c06fcd0
Fujio Kojima <fujiwo@shos.info>
2025-11-14 10:19:09 +0900

    `Animator` クラスを新しいファイルに移動
    `Animator` クラスを `MainForm.cs` から `Animator.cs` に移動しました。
    クラスの内容に変更はありませんが、新しいファイルでは
    `namespace FCAICad` を追加し、名前空間に属するようにしました。

---
+++ FCAICad1/Animator.cs
@@ -0,0 +1,22 @@
+﻿namespace FCAICad;
+
+public static class Animator
+{
+    /// <summary>1 フレームの時間を指定してアニメーション機能を提供</summary>
+    /// <param name="interval">1 フレームの時間をミリ秒単位で指定</param>
+    /// <param name="callback">
+    /// bool callback(int frame) の形でコールバックを指定
+    /// frame は 0 から 1 ずつ増加
+    /// </param>
+    public static void Animate(int interval, Func<int, bool> callback)
+    {
+        var timer = new System.Windows.Forms.Timer { Interval = interval };
+        var frame = 0;
+        timer.Tick += (_, _) => {
+            if (!callback(frame))
+                timer.Stop();
+            frame++;
+        };
+        timer.Start();
+    }
+}
